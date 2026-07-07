29305aca38ffa529711c8a26cf537d060973588f
aman-senpai <jemscollin1@gmail.com>
2026-07-07 22:51:10 +0530

    Initial Commit

---
+++ MacLaunch/MacLaunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacLaunchApp.swift
+//  MacLaunch
+//
+//  Created by Aman Raj on 7/7/26.
+//
+
+import SwiftUI
+
+@main
+struct MacLaunchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
